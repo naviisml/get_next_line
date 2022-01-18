9f17237f769ab490970ff2a3653596a7b17e5dc3
Navi <mailvoornavi@gmail.com>
2022-01-18 13:30:59 +0100

    working version for gnl

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -13,8 +13,30 @@
 #include "get_next_line.h"
 
 /*
+ * The find_next_line() function ...
+ */
+
+static int	find_next_line(char *data)
+{
+	int	i;
+
+	i = 0;
+	while (data[i] != '\0')
+	{
+		if (data[i] == '\n')
+		{
+			i += 1;
+			break ;
+		}
+		i++;
+	}
+	return (i);
+}
+
+/*
  * The read_next_line() function ...
  */
+
 static char	*read_next_line(char *data)
 {
 	int		i;
@@ -22,11 +44,7 @@ static char	*read_next_line(char *data)
 	char	*line;
 
 	line = NULL;
-	i = 0;
-	while (data[i] != '\0')
-		i++;
-	if (data[i] == '\n')
-		i += 1;
+	i = find_next_line(data);
 	n = 0;
 	if (!data)
 		return (NULL);
@@ -46,17 +64,14 @@ static char	*read_next_line(char *data)
 /*
  * The move_next_line() function ...
  */
+
 static char	*move_next_line(char *data)
 {
 	char	*tmp;
 	int		i;
 	int		n;
 
-	i = 0;
-	while (data[i] != '\0')
-		i++;
-	if (data[i] == '\n')
-		i += 1;
+	i = find_next_line(data);
 	n = 0;
 	if (!data[i])
 	{
@@ -64,6 +79,8 @@ static char	*move_next_line(char *data)
 		return (NULL);
 	}
 	tmp = (char *)malloc((ft_strlen(data) - i + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
 	while (data[i + n])
 	{
 		tmp[n] = data[i + n];
@@ -75,8 +92,9 @@ static char	*move_next_line(char *data)
 }
 
 /*
- * The open_file() function ...
+ * The read_file() function ...
  */
+
 static char	*open_file(int fd, char *data)
 {
 	char	*fdata;
@@ -103,42 +121,16 @@ static char	*open_file(int fd, char *data)
 }
 
 /*
- * The search_files() function ...
- */
-static t_files	*search_files(int fd)
-{
-	static t_files	files;
-	t_files			tmp_file;
-
-	if (files.fd != fd)
-	{
-		files.fd = fd;
-		files.data = "1";
-		files.next = NULL;
-	}
-	else
-	{
-		tmp_file.fd = fd;
-		tmp_file.data = "2";
-		tmp_file.next = NULL;
-		files.next = &tmp_file;
-	}
-	return (&files);
-}
-
-/*
  * The get_next_line() function reads untill the first occurrence of
  * the '\n' or '\0' and returns the next line found, if no line can be
  * found, or if a error has occurred, the function returns NULL
  */
+
 char	*get_next_line(int fd)
 {
-	t_files		*file;
 	static char	*data;
 	char		*line;
 
-	file = search_files(fd);
-	printf("%s\n", file->data);
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	data = open_file(fd, data);
